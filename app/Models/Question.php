feedf874c1c141bc8eeef9dbfa3fe04343b6074a
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-09 16:34:50 -0300

    :gear: creating answer table

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -35,8 +35,8 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if($this->answers>0){
-            if($this->best_anwser_id){
+        if ($this->answers_count > 0) {
+            if ($this->best_anwser_id) {
                 return "answered-accepted";
             }
             return "answered";
@@ -47,5 +47,10 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return Parsedown::instance()->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 }
