ced2feae686b127ef93a0abb631932d8e091f486
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-10 21:16:35 -0300

    :hammer: implement DRY principle on the application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -33,30 +33,11 @@
                                 <i class="fa-solid fa-caret-down fa-3x"></i>
                             </a>
 
-                            <form id="down-vote-answer-{{ $answer->id }}"
-                                action="/answers/{{ $answer->id }}/vote" method="POST" class="d-none">
+                            <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote"
+                                method="POST" class="d-none">
                                 @csrf
                                 <input type="hidden" name="vote" value="-1">
                             </form>
-
-                            @can('accept', $answer)
-                                <a href="#" title="Mark this answer as best answer"
-                                    class="{{ $answer->status }} mt-2"
-                                    onclick="event.preventDefault();document.getElementById('accept-answer-{{ $answer->id }}').submit();">
-                                    <i class="fa-solid fa-check fa-2x"></i>
-                                </a>
-                                <form id="accept-answer-{{ $answer->id }}"
-                                    action="{{ route('answers.accept', $answer->id) }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            @else
-                                @if ($answer->is_best)
-                                    <a href="#" title="The question owner accepted this answer as best answer"
-                                        class="{{ $answer->status }} mt-2">
-                                        <i class="fa-solid fa-check fa-2x"></i>
-                                    </a>
-                                @endif
-                            @endcan
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
@@ -89,19 +70,10 @@
                                 </div>
 
                                 <div class="col-4">
-                                    <span class="text-muted">
-                                        Answered {{ $answer->created_date }}
-                                    </span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}" alt="User avatar">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">
-                                                {{ $answer->user->name }}
-                                            </a>
-                                        </div>
-                                    </div>
+                                    @include('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answered',
+                                    ])
                                 </div>
                             </div>
 
